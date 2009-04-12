ee795bcac47df5ad96d8182d4291d6f2e480d844
xian <xian@mintchaos.com>
2009-04-12 13:00:38 -0500

    Improved docstring for the youtube inline.

--- django_inlines/samples.py
+++ django_inlines/samples.py
@@ -4,6 +4,15 @@ from django_inlines.inlines import TemplateInline
 class YoutubeInline(TemplateInline):
     """
     An inline that takes a youtube URL or video id and returns the proper embed.
+    
+    Examples::
+    
+        {{ youtube http://www.youtube.com/watch?v=4R-7ZO4I1pI&hd=1 }}
+        {{ youtube 4R-7ZO4I1pI }}
+        
+    The inluded template supports width and height arguments::
+    
+        {{ youtube 4R-7ZO4I1pI width=850 height=500 }}
     """
     def get_context(self):
         video_id = self.value
