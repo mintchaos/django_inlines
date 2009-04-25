fd52cef2bab698bb2f0bb98a6c94c5cc20acb807
xian <xian@mintchaos.com>
2009-04-25 14:56:01 -0500

    Rolling 0.7.1 - the actually works on Pypi version! Thanks Jannis!

---
+++ CHANGES.rst
@@ -0,0 +1,13 @@
+Changes:
+========
+
+0.7.1
+*****
+
+* Includes fixes from Jannis Leidel so it actually works on Pypi now. Thanks Jannis!
+
+
+0.7
+***
+
+* Initial public release
--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ from distutils.core import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+README = read('README.rst')
+CHANGES = read('CHANGES.rst')
 
 setup(
     name = "django_inlines",
-    version = "0.7",
+    version = "0.7.1",
     url = 'http://github.com/mintchaos/django_inlines',
     license = 'BSD',
     description = "For embedding anything you'd like into text in your django apps.",
-    long_description = read('README.rst'),
+    long_description='\n\n\n'.join([README, CHANGES]),
     
     author = 'Christian Metts',
     author_email = 'xian@mintchaos.com',
